6c2436a906fd961dba6ccfb953c100bf7161fdaa
Damian Niedziółka <damianniedziolka7@gmail.com>
2025-10-14 19:54:22 +0200

    Add meta files for new assets and interfaces

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,23 +12,28 @@ namespace Player
         private InputSystem_Actions _inputActions;
         private InputSystem_Actions.PlayerActions _playerActions;
         
+        [Header("Components")]
         private Rigidbody _rb;
         private Collider _colli;
+        private Camera _playerCamera;
         
+        [Header("Private variables")]
         private Vector2 _inputVector;
-        [SerializeField] private bool isGrounded;
+        private bool isGrounded;
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             _rb = GetComponent<Rigidbody>();
             _colli = GetComponent<Collider>();
+            _playerCamera = Camera.main;
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
-            Vector3 moveVector = new Vector3(_inputVector.x, 0, _inputVector.y);
+            Vector3 moveVector = _inputVector.y * transform.forward + _inputVector.x * transform.right;
+            
             _rb.AddForce(moveVector * 10, ForceMode.Force);
         }
 
@@ -36,6 +41,12 @@ namespace Player
         {
             isGrounded = Physics.Raycast(transform.position, Vector3.down, _colli.bounds.extents.y + 0.1f);
             Debug.DrawRay(transform.position, Vector3.down * (_colli.bounds.extents.y + 0.1f), Color.red);
+            if(_playerCamera)
+            {
+                _playerCamera.transform.position = transform.position + new Vector3(0, 1f, 0);
+                _playerCamera.transform.rotation = transform.rotation;
+            }
+            
         }
 
         private void OnEnable()
@@ -45,16 +56,18 @@ namespace Player
             _playerActions.Enable();
             _playerActions.Jump.performed += JumpInput;
             _playerActions.Jump.canceled += JumpInput;
-            _playerActions.Move.performed += Move;
-            _playerActions.Move.canceled += Move;
+            _playerActions.Move.performed += MoveInput;
+            _playerActions.Move.canceled += MoveInput;
+            _playerActions.Look.performed += LookInput;
         }
         
         private void OnDisable()
         {
             _playerActions.Jump.performed -= JumpInput;
             _playerActions.Jump.canceled -= JumpInput;
-            _playerActions.Move.performed -= Move;
-            _playerActions.Move.canceled -= Move;
+            _playerActions.Move.performed -= MoveInput;
+            _playerActions.Move.canceled -= MoveInput;
+            _playerActions.Look.performed -= LookInput;
             _playerActions.Disable();
         }
 
@@ -65,16 +78,25 @@ namespace Player
             }
         }
         
+        private void LookInput(InputAction.CallbackContext context)
+        {
+            Vector2 lookVector = context.ReadValue<Vector2>();
+            transform.Rotate(0, lookVector.x * 0.1f, 0);
+            if(_playerCamera)
+            {
+                _playerCamera.transform.Rotate(-lookVector.y * 0.1f, 0, 0);
+            }
+        }
+        
         private void JumpInput(InputAction.CallbackContext context)
         {
-                Debug.Log("Jump");
                 if(context.performed)
                     InvokeRepeating(nameof(Jump), 0, 0.1f);
                 if(context.canceled)
                     CancelInvoke(nameof(Jump));
         }
         
-        private void Move(InputAction.CallbackContext context)
+        private void MoveInput(InputAction.CallbackContext context)
         {
             _inputVector = context.ReadValue<Vector2>();
         }
