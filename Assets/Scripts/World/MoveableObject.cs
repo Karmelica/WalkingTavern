3869e547c16400cd58596d8353dede6a7c36a074
Damian Niedziółka <damianniedziolka7@gmail.com>
2025-12-05 17:47:25 +0100

    Update ownership settings in prefabs and adjust ownership request logic

--- Assets/Scripts/World/MoveableObject.cs
+++ Assets/Scripts/World/MoveableObject.cs
@@ -59,6 +59,7 @@ namespace World
         private void SetTransformsServerRpc(NetworkBehaviourReference interactor, bool pickingUp = true)
         {
             if (!interactor.TryGet(out Player.Player player)) return;
+            NetworkObject.RequestOwnership();
             _isInteractedWith.Value = pickingUp;
             _rigidbody.useGravity = !_isInteractedWith.Value;
             _rigidbody.maxLinearVelocity = _isInteractedWith.Value ? float.MaxValue : _rigidbody.maxLinearVelocity = 5f;
@@ -71,7 +72,6 @@ namespace World
 
         public void PrimaryInteract(NetworkBehaviourReference interactor, bool pickingUp = true)
         {
-            NetworkObject.RequestOwnership();
             SetTransformsServerRpc(interactor, pickingUp);
         }
 
