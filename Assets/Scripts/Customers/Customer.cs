9f79f9f00fd4c8290436b4b6fa63303cb1681c5e
Damian Niedziółka <damianniedziolka7@gmail.com>
2026-01-16 16:17:43 +0100

    Simple AI

--- Assets/Scripts/Customers/Customer.cs
+++ Assets/Scripts/Customers/Customer.cs
@@ -2,17 +2,18 @@ using System;
 using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Customer : NetworkBehaviour
 {
+    [SerializeField] private NavMeshAgent agent;
     [SerializeField] private CharacterController controller;
-    private NetworkVariable<Vector3> randomDestination = new NetworkVariable<Vector3>(new Vector3(0,0,0));
-    [SerializeField] private Vector3 newDestination;
-    private float changeDirectionInterval = 5f;
-    private float _timer = 0f;
     [SerializeField] private bool isMoving = true;
-    private float offset = 20f;
-    private float moveSpeed = 3f;
+    
+    private NetworkVariable<Vector3> _randomDestination = new NetworkVariable<Vector3>(new Vector3(0,0,0));
+    
+    private const float ChangeDirectionInterval = 5f;
+    private const float Offset = 20f;
 
     private void Start()
     {
@@ -26,25 +27,24 @@ public class Customer : NetworkBehaviour
 
     private void Move()
     {
-        controller.Move((randomDestination.Value - transform.position).normalized * (Time.deltaTime * moveSpeed));
+        agent.SetDestination(_randomDestination.Value);
         if (controller.isGrounded) return;
         controller.Move(Physics.gravity * Time.deltaTime);
     }
 
     private IEnumerator PickRandomDirection()
     {
-        var wait = new WaitForSeconds(changeDirectionInterval);
+        var wait = new WaitForSeconds(ChangeDirectionInterval);
         while (isMoving)
         {
             Vector3 destination = new Vector3(RandomRange(transform.position.x), 0, RandomRange(transform.position.z));
-            newDestination = destination;
-            randomDestination.Value = destination;
+            _randomDestination.Value = destination;
             yield return wait;
         }
     }
 
     private float RandomRange(float pos)
     {
-        return UnityEngine.Random.Range(pos - offset, pos + offset);
+        return UnityEngine.Random.Range(pos - Offset, pos + Offset);
     }
 }
