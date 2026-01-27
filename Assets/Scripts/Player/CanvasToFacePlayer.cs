d4dbcdef69f4333739cddfd4aacbe016746aaf92
Damian Niedziółka <damianniedziolka7@gmail.com>
2026-01-27 19:57:37 +0100

    27.01

--- Assets/Scripts/Player/CanvasToFacePlayer.cs
+++ Assets/Scripts/Player/CanvasToFacePlayer.cs
@@ -6,6 +6,7 @@ public class CanvasToFacePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!Camera.main) return;
         transform.forward = Camera.main.transform.forward;
     }
 }
