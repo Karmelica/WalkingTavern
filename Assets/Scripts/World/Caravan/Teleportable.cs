024f487a337b02509a6be1eef9cda6f8bf65f1fa
Damian Niedziółka <damianniedziolka7@gmail.com>
2026-05-04 20:53:50 +0200

    Teleportable

--- Assets/Scripts/World/Caravan/Teleportable.cs
+++ Assets/Scripts/World/Caravan/Teleportable.cs
@@ -1,11 +1,26 @@
+using System;
+using Unity.Netcode;
+using Unity.Netcode.Components;
 using UnityEngine;
 
 namespace World.Caravan
 {
-    public class Teleportable : MonoBehaviour
+    public class Teleportable : NetworkBehaviour
     {
+        private NetworkTransform _trans;
+
+        private void Awake() => _trans = GetComponent<NetworkTransform>();
+        
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            if (!IsOwner) enabled = false;
+        }
+
         public void Teleport(Vector3 position, Quaternion rotation)
         {
+            if (!IsOwner) return;
+            _trans.Teleport(position, rotation, Vector3.one);
             transform.SetPositionAndRotation(position, rotation);
             Physics.SyncTransforms();
         }
